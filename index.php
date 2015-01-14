973509c3356a36396f685099cb5ff5028b408b14
_remc <rene.mejiac@gmail.com>
2015-01-13 20:28:30 -0800

    Primer commit
    Definiendo estilo del app

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>gltest14</title>
+    <link rel="stylesheet" href="resources/css/style.css" />
+    <link rel="stylesheet" href="resources/css/style.min.css" />
+    <link rel="stylesheet" href="resources/css/style.css.map" />
+    <link rel="stylesheet" href="resources/css/style-theme.css" />
+    <link rel="stylesheet" href="resources/css/style-theme.min.css" />
+    <link rel="stylesheet" href="resources/css/style-theme.min.map" />
+    <script charset="utf-8">
+      function Comenzar(){
+        location.href="start"
+      }
+    </script>
+
+  </head>
+  <body>
+<!--============================================================================-->
+<!--============================ Barra de Navegación ===========================-->
+<!--============================================================================-->
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="https://plus.google.com/u/0/+ReneMejia8/">Ren&eacute; Enrique Mej&iacute;a</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Men&uacute; Principal</a></li>
+        <li><a><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> </a></li>
+        <li><a><span class="glyphicon glyphicon-file" aria-hidden="true"></span> </a></li>
+        <li><a><span class="glyphicon glyphicon-floppy-saved" aria-hidden="true"></span> </a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!--============================================================================-->
+<!--============================== Head del Sitio ==============================-->
+<!--============================================================================-->
+<div><!-- div principal -->
+
+  <div class="col-md-10">
+    <img alt="" src="resources/img/glogo.png" style="line-height: 20.799999237060px; float: left; width: 200px; height: 144px;" />
+    <h3>Formulario con ABC</h3>
+    <h4>Evaluaci&oacute;n para vacante de programador PHP</h4>
+  </div>
+<!--============================================================================-->
+<!--================================= Contenido ================================-->
+<!--============================================================================-->
+  <div>
+
+    <div class="col-md-10-offset-3 container" style="position: middle;">
+      <h1>Bienvenido!<br><h4><small>13 de Enero del 2015</h4></h1>
+        <h2><small>1. En esta secci&oacute;n describir&eacute; el funcionamiento de la aplicaci&oacute;n.</small></h2>
+        <h2><small>2. Tratar&eacute; de ser breve y a la vez elegante.</h2></small>
+        <h2><small>3. El footer se est&aacute; poniendo punk, seg&uacute;n veo.</h2></small>
+
+        <br>
+
+      <button type="button" onclick="location='start'" class="btn btn-default btn-lg" href="start.php"><span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span> Ir a la aplicaci&oacute;n</button>
+
+    </div>
+
+</div>
+
+<!-- Se hacen saltos de línea en caso de que el footer -->
+<!-- se coma parte del espacio del contenido. -->
+<br><br><br><br><br>
+<!--============================================================================-->
+<!--================================= Footer ===================================-->
+<!--============================================================================-->
+</div> <!-- div principal -->
+
+<footer>
+  <div class="container">
+    <p>@_remc 2015 <a href=""> copyright</a></p>
+  </div>
+</footer>
+
+  <script src="resources/js/style.js"></script>
+  <script src="resources/js/style.min.js"></script>
+  <script src="resources/js/npm.js"></script>
+  <script>
+    $('.dropdown-toggle').dropdown()
+  </script>
+  </body>
+</html>
---
+++ start.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>gltest14</title>
+    <link rel="stylesheet" href="resources/css/style.css" />
+    <link rel="stylesheet" href="resources/css/style.min.css" />
+    <link rel="stylesheet" href="resources/css/style.css.map" />
+    <link rel="stylesheet" href="resources/css/style-theme.css" />
+    <link rel="stylesheet" href="resources/css/style-theme.min.css" />
+    <link rel="stylesheet" href="resources/css/style-theme.min.map" />
+  </head>
+  <body>
+<!--============================================================================-->
+<!--============================ Barra de Navegación ===========================-->
+<!--============================================================================-->
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="https://plus.google.com/u/0/+ReneMejia8/">Ren&eacute; Enrique Mej&iacute;a</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Men&uacute; Principal</a></li>
+        <li><a><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> </a></li>
+        <li><a><span class="glyphicon glyphicon-file" aria-hidden="true"></span> </a></li>
+        <li><a><span class="glyphicon glyphicon-floppy-saved" aria-hidden="true"></span> </a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!--============================================================================-->
+<!--============================== Head del Sitio ==============================-->
+<!--============================================================================-->
+<div><!-- div principal -->
+
+  <div class="col-md-10">
+    <img alt="" src="resources/img/glogo.png" style="line-height: 20.799999237060px; float: left; width: 200px; height: 144px;" />
+    <h3>Formulario con ABC</h3>
+    <h4>Evaluaci&oacute;n para vacante de programador PHP</h4>
+  </div>
+<!--============================================================================-->
+<!--================================= Contenido ================================-->
+<!--============================================================================-->
+<div>
+  <div class="col-md-10-offset-3 container" style="position: middle;">
+
+    <h1>Men&uacute; Principal</h1>
+
+    <div class="list-group">
+      <a href="index" class="list-group-item">
+        <h4 class="list-group-item-heading">Dar de alta un usuario en el sistema</h4>
+        <p class="list-group-item-text">Dar clic en esta seccion para ir a altas de usuario.</p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Dar de baja un usuario en el sistema</h4>
+        <p class="list-group-item-text">Dar clic en esta seccion para ir a altas de usuario.</p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Modificar los datos de un usuario en el sistema</h4>
+        <p class="list-group-item-text">Dar clic en esta seccion para ir a altas de usuario.</p>
+      </a>
+    </div>
+
+  </div>
+</div>
+
+  <!-- Se hacen saltos de línea en caso de que el footer -->
+  <!-- se coma parte del espacio del contenido. -->
+  <br><br><br><br><br><br><br>
+<!--============================================================================-->
+<!--================================= Footer ===================================-->
+<!--============================================================================-->
+  </div> <!-- div principal -->
+
+<footer>
+  <div class="container">
+    <p>@_remc 2015 <a href=""> copyright</a></p>
+  </div>
+</footer>
+
+  <script src="resources/js/style.js"></script>
+  <script src="resources/js/style.min.js"></script>
+  <script src="resources/js/npm.js"></script>
+  <script>
+    $('.dropdown-toggle').dropdown()
+  </script>
+  </body>
+</html>
